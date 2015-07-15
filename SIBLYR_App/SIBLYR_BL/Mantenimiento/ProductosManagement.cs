5ad8fbad030875499fcca00eb2584a109969562a
Roberto2504 <robertoaf19@gmail.com>
2015-07-15 07:34:40 -0600

    Cambios
    Conexion BD, Inicio Mantenimiento Producto

---
+++ SIBLYR_App/SIBLYR_BL/Mantenimiento/ProductosManagement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data;
+using System.Collections.ObjectModel;
+using System.Collections;
+namespace SIBLYR_BL.Mantenimiento
+{
+    public class ProductosManagement
+    {
+
+    }
+}
---
+++ SIBLYR_App/SIBLYR_BO/SIBLYR_DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace SIBLYR_BO
+{
+   public partial class SIBLYR_ModelDataContext
+    {
+        public SIBLYR_ModelDataContext() :
+            base(ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString, mappingSource)
+        {
+            OnCreated();
+        }
+    }
+}
